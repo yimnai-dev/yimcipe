023843fe4a0be3d3ac86bf7b0f2b5325ae77c232
Nerus-Zaumu <neruszaumuy@gmail.com>
2023-03-17 19:25:28 +0100

    Changed origin in api for frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -16,7 +16,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4200', credentials: true });
+  app.enableCors({ origin: 'https://yimla.site', credentials: true });
   const globalPrefix = 'api/v1.0';
   const configService = app.get(ConfigService);
   app.setGlobalPrefix(globalPrefix);
