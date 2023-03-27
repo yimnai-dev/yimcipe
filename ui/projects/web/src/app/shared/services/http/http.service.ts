f2cfc14eda327290453eb207a81470d23b779788
Nerus-Zaumu <neruszaumuy@gmail.com>
2023-03-27 16:52:09 +0100

    Changed baseUrl

--- ui/projects/web/src/app/shared/services/http/http.service.ts
+++ ui/projects/web/src/app/shared/services/http/http.service.ts
@@ -8,8 +8,7 @@ export class HttpService {
 
   constructor(private httpClient: HttpClient){}
 
-  // baseUrl = 'http://api.yimla.site:3333/api/v1.0'
-  baseUrl = 'http://localhost:3333/api/v1.0'
+  baseUrl = 'http://api.yimla.site:3333/api/v1.0'
 
   httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json'}),
